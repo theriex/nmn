4da6a6d385ab17bab210745573199258d3ce6700
Eric Parker <theriex@gmail.com>
2017-02-20 23:07:18 -0500

    Basic site framework

---
+++ docroot/js/app.js
@@ -0,0 +1,64 @@
+/*jslint browser, multivar, white, fudge */
+/*global window jtminjsDecorateWithUtilities */
+
+var app = {},  //Global container for application level funcs and values
+    jt = {};   //Global access to general utility methods
+
+(function () {
+    "use strict";
+
+    var pages = [{id:"artdiv", name:"Notable Articles",
+                  content:"<div id=\"membicdiv\" style=\"height:90vh;\"><iframe id=\"membiciframe\" src=\"https://membic.com?view=coop&coopid=6385940616445952&css=none&site=notmynormal.org\" style=\"position:relative;height:100%;width:100%\" seamless=\"seamless\" frameborder=\"0\"/></iframe></div>"},
+                 {id:"caldiv", name:"UMB Calendar",
+                  content:"<iframe src=\"https://calendar.google.com/calendar/embed?src=notmynormal.org%40gmail.com&ctz=America/New_York\" style=\"border: 0\" width=\"800\" height=\"600\" frameborder=\"0\" scrolling=\"no\" id=\"califrame\"></iframe>"}];
+
+
+    function makeTabs () {
+        var html = [];
+        pages.forEach(function (pg, idx) {
+            html.push(["div", {id: pg.id + "tab", 
+                               cla: (idx? "tabdiv" : "seltabdiv"),
+                               onclick: jt.fs("app.tabsel(" + idx + ")")},
+                       pg.name]); });
+        jt.out("tabcontdiv", jt.tac2html(html));
+    }
+
+
+    function makePages () {
+        var html = [];
+        pages.forEach(function (pg, idx) {
+            html.push(["div", {id: pg.id,
+                               cla: (idx? "pagediv" : "selpagediv")},
+                       pg.content]); });
+        jt.out("pagecontentdiv", jt.tac2html(html));
+    }
+
+
+    function adjustHeight () {
+        var h = jt.byId("contentdiv").offsetHeight;
+        h -= jt.byId("titlediv").offsetHeight;
+        h -= 10; //titlediv margin
+        h -= jt.byId("tabcontdiv").offsetHeight;
+        jt.byId("pagecontentdiv").style.height = String(h) + "px";
+    }
+
+
+    app.tabsel = function (selidx) {
+        pages.forEach(function (pg, idx) {
+            if(idx === selidx) {
+                jt.byId(pg.id + "tab").className = "seltabdiv";
+                jt.byId(pg.id).className = "selpagediv"; }
+            else {
+                jt.byId(pg.id + "tab").className = "tabdiv";
+                jt.byId(pg.id).className = "pagediv"; } });
+    };
+
+
+    app.init = function () {
+        jtminjsDecorateWithUtilities(jt);
+        adjustHeight();
+        makeTabs();
+        makePages();
+    };
+
+}());
