8dd6f4b4da06f9684f6c6bfa70a550b33d449f93
Eric Parker <theriex@gmail.com>
2017-03-11 13:25:02 -0500

    straight iframe for membic include, fixed css for 100% height

--- docroot/js/app.js
+++ docroot/js/app.js
@@ -8,7 +8,7 @@ var app = {},  //Global container for application level funcs and values
     "use strict";
 
     var pages = [{id:"artdiv", name:"Notable Articles",
-                  content:"<div id=\"membicdiv\" style=\"height:90vh;\"><iframe id=\"membiciframe\" src=\"https://membic.com?view=coop&coopid=6385940616445952&css=none&site=notmynormal.org\" style=\"position:relative;height:100%;width:100%\" seamless=\"seamless\" frameborder=\"0\"/></iframe></div>"},
+                  content:"<iframe id=\"membiciframe\" src=\"https://membic.com?view=coop&coopid=6385940616445952&css=none&site=notmynormal.org\" style=\"position:relative;height:100%;width:100%\" seamless=\"seamless\" frameborder=\"0\"/></iframe>"},
                  {id:"caldiv", name:"UMB Calendar",
                   content:"<iframe src=\"https://calendar.google.com/calendar/embed?src=notmynormal.org%40gmail.com&ctz=America/New_York\" style=\"border: 0\" width=\"$WIDTH\" height=\"600\" frameborder=\"0\" scrolling=\"no\" id=\"califrame\"></iframe>"}];
 
